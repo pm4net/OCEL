dd41ec48e0c7c5dc6483c10f61ddb7c71df0c67c
Johannes Mols <johannes.mols@kraftvaerk.com>
2022-11-18 21:42:12 +0100

    Added console app to generate C# file from JSON schema

---
+++ Generator/Program.cs
@@ -0,0 +1,7 @@
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+var schema = await JsonSchema.FromFileAsync(@"..\..\..\..\Schemas\schema.json");
+var generator = new CSharpGenerator(schema);
+var file = generator.GenerateFile();
+await File.WriteAllTextAsync("OcelLog.cs", file);
