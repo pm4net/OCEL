82c12e54af06d18d74b467fab09d3c06caed1427
Johannes Mols <johannes.mols@kraftvaerk.com>
2022-11-20 22:00:10 +0100

    Restructured project slightly

---
+++ OCEL/Xml.fs
@@ -0,0 +1,19 @@
+﻿namespace OCEL
+
+open OCEL.Types
+
+module Xml =
+    [<Literal>]
+    let private SchemaXml = ""
+
+    let Validate xml =
+        true
+
+    let ValidateWithErrorMessages xml =
+        true, []
+
+    let Deserialize xml =
+        []
+
+    let Serialize log =
+        ""
